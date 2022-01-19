fbb5026992a5c2799ae8ff9b35b934a482ca2ba0
Shouvik Ghosh <01.shouvik@gmail.com>
2022-01-19 17:24:16 +0530

    stable release

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, Response, render_template, session, request
+from flask import Flask, Response, render_template, session, request,jsonify
 from random import randint
 import json
 import time
 import pandas as pd
 app = Flask(__name__)
 
-def get_message():
-    '''this could be any function that blocks until data is ready'''
-    time.sleep(1.0)
-    s = time.ctime(time.time())
-    return s
 
 def event_stream():
     data_dic = {}
@@ -18,7 +13,6 @@ def event_stream():
         time.sleep(0.05)
         data = json.dumps(pyth_data.to_dict(orient="records")) 
         yield f"data:{data}\n\n"
-
 
 @app.route("/")
 def index():
@@ -39,6 +33,15 @@ def elements():
 @app.route("/stream")
 def stream():
     return Response(event_stream(), mimetype="text/event-stream")
+
+@app.route("/get_index_data")
+def ret_index_data():
+    data = pd.read_csv("index.csv")
+    index_list = data.set_index('Index_name').T.to_dict('list')
+    for key, value in index_list.items():
+        value = [x for x in value if x == x]
+        index_list[key] = value
+    return jsonify(index_list)
 
 
 if __name__ == "__main__":
--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -1,44 +1,67 @@
-let index = ['Crypto.LTC/USD','Crypto.BTC/USD', 'Equity.US.QQQ/USD'];
-				let dict;
-				function index1(){
-					index = ['Crypto.LTC/USD'];
-				}
-				function index2(){
-					index = ['Crypto.LTC/USD','Crypto.BTC/USD', 'Equity.US.QQQ/USD'];
-				}
-				function index3(){
-					index = ['Crypto.LTC/USD','Crypto.BTC/USD'];
-				}
-				function index4(){
-					index = ['Crypto.LTC/USD'];
-				}
-				var table = document.getElementById("pyth-data-table");
-				var eventSource = new EventSource("/stream")
-				eventSource.onmessage = function(e) {
-					dict = JSON.parse(e.data);
-					var tbody = document.getElementById("pyth-data-table-tbody")
-					tbody.innerHTML = ""
-					for (var i = 1; i < dict.length; i++){
-						  
-						if(index.includes(dict[i]['Market'])){
-							var tr = document.createElement('tr'); 
-							var td1 = document.createElement('td');
-							var td2 = document.createElement('td');
-							var td3 = document.createElement('td');
-							var text1 = document.createTextNode(dict[i]['Market']);
-							var text2 = document.createTextNode(parseFloat(dict[i]['Value']).toFixed(3)+' +/- '+ parseFloat(dict[i]['Confidence']).toFixed(2));
-							var text3 = document.createTextNode(" ");
-							td1.appendChild(text1);
-							td2.appendChild(text2);
-							td3.appendChild(text3);
-							tr.appendChild(td1);
-							tr.appendChild(td2);
-							tr.appendChild(td3);
-							tbody.appendChild(tr);
-						}
-					}
-					table.appendChild(tbody);
+var index, dict, index_list, index1, index2;
+var index3, index4, table, eventSource;
 
-				};
-			
-			
+// Getting the index data
+function get_index_data(){
+    fetch('/get_index_data')
+    .then((response)=> response.json())
+    .then((index_data) => {
+        index_list = index_data;
+        // setting the value of index for the first page load.
+        index = index_list['index1'];
+    });
+    
+}
+
+function selectIndex(index_name){
+    
+    if(index_name){
+        index = index_list[index_name];
+    }
+}
+//Eventlisteners for button clicks
+index1 = document.getElementById("index1");
+index1.addEventListener('click',function(){
+    selectIndex('index1');
+});
+index2 = document.getElementById("index2");
+index2.addEventListener('click',function(){
+    selectIndex('index2');
+});
+index3 = document.getElementById("index3");
+index3.addEventListener('click',function(){
+    selectIndex('index3');
+});
+index4 = document.getElementById("index4");
+index4.addEventListener('click',function(){
+    selectIndex('index4');
+});
+
+table = document.getElementById("pyth-data-table");
+eventSource = new EventSource("/stream")
+eventSource.onmessage = function(e) {
+    dict = JSON.parse(e.data);
+    var tbody = document.getElementById("pyth-data-table-tbody")
+    tbody.innerHTML = ""
+    for (var i = 1; i < dict.length; i++){
+        if(index && index.includes(dict[i]['Market'])){
+            var tr = document.createElement('tr'); 
+            var td1 = document.createElement('td');
+            var td2 = document.createElement('td');
+            var td3 = document.createElement('td');
+            var text1 = document.createTextNode(dict[i]['Market']);
+            var text2 = document.createTextNode(parseFloat(dict[i]['Value']).toFixed(3)+' +/- '+ parseFloat(dict[i]['Confidence']).toFixed(2));
+            var text3 = document.createTextNode(" ");
+            td1.appendChild(text1);
+            td2.appendChild(text2);
+            td3.appendChild(text3);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tbody.appendChild(tr);
+        }
+    }
+    table.appendChild(tbody);
+
+};
+
